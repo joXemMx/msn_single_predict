fdeeeec204be0c589cf795ee2f900a0f6215b4af
joXemMx <jonas.em@t-online.de>
2023-09-01 19:20:24 +0200

    copy smiles_config

--- msnovelist/fp_management/__init__.py
+++ msnovelist/fp_management/__init__.py
@@ -7,4 +7,4 @@ from .fp_database import *
 from .fp_database_csv import *
 from .fp_database_hdf5 import *
 from .fp_database_sqlite import *
-from ..smiles_config import *
+from .smiles_config import *
